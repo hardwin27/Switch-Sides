7807ca75f9e3cdfd67499890ebe7b8f0335a5946
Hardwin Welly <hardwin27@gmail.com>
2020-05-17 19:14:08 +0800

    Progress: Add new color scheme. If player git RedTile, player die.

--- Characters/Player.gd
+++ Characters/Player.gd
@@ -3,7 +3,15 @@ extends "res://Characters/Character.gd"
 	
 func _physics_process(delta: float) ->void:
 	move_and_slide(_speed, _FLOOR_NORMAL)
+	is_collide_with_deadly_object()
 
 func _input(event: InputEvent) ->void:
-	if event.is_action_pressed("switch_sides"):
+	if event.is_action_pressed("switch_sides") and is_on_floor() or is_on_ceiling():
 		_speed.y *= -1
+
+func is_collide_with_deadly_object() -> void:
+	var collision_index = get_slide_count()
+	if collision_index > 0:
+		var collide_with: = get_slide_collision(collision_index - 1)
+		if collide_with.collider.name == "RedPlatform":
+			queue_free()
